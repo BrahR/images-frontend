5fd15b159f02d268d557049de7b00f2093c48766
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-08-24 22:28:45 +0200

    Its working enough

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,67 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Sogga Image Uploader</title>
-				<meta name="description" content="very cool" />
+				<meta name="description" content="A image uploader made to be quick" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href="https://discord.gg/KkMKCchJb8">Sogga images!</a>
+					Welcome to&nbsp;<a href="https://discord.gg/KkMKCchJb8">Sogga images!</a>
 				</h1>
-
 				<p className={styles.description}>
-					Get started by joining{' '}
-					<a href="https://discord.gg/mY8zTARu4g">the discord</a> and pinging
-					tricked to get access
+					Get started by joining&nbsp;<a href="https://discord.gg/mY8zTARu4g">the discord</a>&nbsp;and pinging tricked to get access
 				</p>
-
-				{/* <div className={styles.grid}>
-					<a href="/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>How to use this yourself to upload your own images</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div> */}
+				<div className={styles.description}>
+					{makeCard(
+						"Easily change your settings",
+						"You can do change everything via discord,\nmaking it the perfect image uploader for people who dont want to visit a whole new site to just chane some settings",
+						"https://sogga.herokuapp.com/view/a73a4"
+					)}
+					{makeCard("Changing domains", "You can easily change your domain from inside discord with just 1 slash command,", "https://sogga.herokuapp.com/view/09946")}
+					{makeCard("So what are you waiting for", "Start now! by joining the discord, asking for a code and redeeming it its that simple!", "https://sogga.herokuapp.com/view/6344a")}
+				</div>
 			</main>
 
 			<footer className={styles.footer}>
 				<a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
-					Powered by{' '}
+					Powered by{" "}
 					<span className={styles.logo}>
 						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 					</span>
 				</a>
 			</footer>
+		</div>
+	);
+}
+function makeCard(name: string, content: string, img: string) {
+	return (
+		<div className={styles.item}>
+			<Card  >
+				<CardActionArea>
+					<CardMedia className={styles.image} image={img} title="Contemplative Reptile" />
+					<CardContent className={styles.cardbody}>
+						<Typography gutterBottom variant="h5" component="h2">
+							{name}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							{content}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
 		</div>
 	);
 }
