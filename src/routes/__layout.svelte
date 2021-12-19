ba6205b9075d21827a23b3e4ededc91dbac1cd46
BrahR <44587812+BrahR@users.noreply.github.com>
2021-12-19 10:28:00 -0800

    grid?

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -88,24 +88,6 @@
 			}
 		};
 
-    // let other: HTMLElement = document.querySelector(".other");
-    // let svg: HTMLElement = document.querySelector(".other svg");
-    // let list: HTMLElement = document.querySelector(".ee ul");
-
-    // other.addEventListener("click", () => {
-    //   let listHeight: number = list.scrollHeight;
-    //   console.log(list);
-      
-  	//   if (list.style.height != "0px") {
-  	// 	  list.style.height = "0px";
-  	// 	  svg.style.transform = "rotate(90deg)";
-        
-  	//   } else {
-  	//   	list.style.height = listHeight + "px";
-  	// 	  svg.style.transform = "rotate(0deg)";
-  	//   }
-    // });
-
 		// add events when element is added to the DOM
 		document.addEventListener('click', handleOutsideClick, false);
 		document.addEventListener('keyup', handleEscape, false);
@@ -149,7 +131,7 @@
 </svelte:head>
 
 <template bind:this={menu} class="flex-1 flex flex-col">
-  <nav class="nav">
+  <nav>
     <div class="all">
       <a class="ascella" href="https://ascella.host">Ascella.host</a>
       <div class="btns">
@@ -158,8 +140,7 @@
           <li><a class="btn" href="/"> Home </a></li>
           <li><a class="btn" href="https://discord.gg/mY8zTARu4g" > Discord </a></li>
         </ul>
-      </div>
-      <div class="e">
+              <div class="e">
         <div class="ee">
           <div class="other">
             <input id="input" type="checkbox" checked>
@@ -182,83 +163,13 @@
 				      </ul>      
             </div>
           </div>
-          
         </div>
       </div>
+      </div>
+
       <a class="btn" href="/docs/signup">Create an account</a>
     </div>
   </nav>
-	<nav
-		class="flex items-center justify-between flex-wrap bg-slate-700 py-4 lg:px-12 shadow border-solid border-t-2 border-lime-500"
-	>
-		<div
-			class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0"
-		>
-			<div class="flex items-center flex-shrink-0 text-slate-100 mr-16">
-				<span
-					class="font-semibold text-xl tracking-tight hover:text-lime-500 duration-200 hover:border-b-2 border-indigo-700"
-					><a href="https://ascella.host">Ascella.host</a></span
-				>
-			</div>
-		</div>
-
-		<div class="menu w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-			<div class="lg:text-lg text-md font-bold text-green-500  lg:flex-grow">
-				<a href="https://dash.ascella.host" class="a-btn"> Dashboard </a>
-				<a href="/" class="a-btn"> Home </a>
-				<a href="https://discord.gg/mY8zTARu4g" class="a-btn"> Discord </a>
-
-				<div class="more-css-tips relative inline-block text-left dropdown">
-					<span class="rounded-md shadow-sm">
-            <div
-							class="inline-flex justify-center w-full px-4 py-2 leading-5 bg-slate-500 text-white hover:bg-slate-700 transition duration-150 ease-in-out border border-gray-300 hover:text-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800"
-							aria-haspopup="true"
-							aria-expanded="true"
-							aria-controls="headlessui-menu-items-117"
-						>
-							<span>Other</span>
-							<svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
-                <path
-									fill-rule="evenodd"
-									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-									clip-rule="evenodd"
-								/>
-              </svg>
-						</div>
-          </span>
-					<div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
-						<div
-							class="absolute left-0 w-56 mt-2 origin-top-left  bg-slate-600 border border-gray-500 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-							aria-labelledby="headlessui-menu-button-1"
-							id="headlessui-menu-items-117"
-							role="menu"
-						>
-							<ul class="py-1">
-								{#each otherLinks as link}
-                <li>
-                  <a
-										href={link.href}
-										tabindex="0"
-										class="text-gray-200 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
-										role="menuitem">{link.a}</a
-									>
-                </li>
-								{/each}
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="flex">
-				<a
-					href="/docs/signup"
-					class="a-btn duration-500 border-b-4 border-blue-300 hover:border-orange-600 lg:text-lg text-md block px-4 py-2 rounded text-green-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0"
-					>Create an account</a
-				>
-			</div>
-		</div>
-	</nav>
 
 	<div class="pb-4" />
 	<slot />
