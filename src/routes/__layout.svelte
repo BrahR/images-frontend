359c3b02e2d3bfc8d28530e4890e427c1bbf54f0
BrahR <44587812+BrahR@users.noreply.github.com>
2021-12-20 02:31:13 -0800

    e

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -140,33 +140,32 @@
           <li><a class="btn" href="/"> Home </a></li>
           <li><a class="btn" href="https://discord.gg/mY8zTARu4g" > Discord </a></li>
         </ul>
-              <div class="e">
-        <div class="ee">
-          <div class="other">
-            <input id="input" type="checkbox" checked>
-            <span>Other
-              <svg class="w-5 h-5 -mr-1" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-              </svg>
-            </span>
-            <div class="list">
-              <ul>
-				  	  {#each otherLinks as link}
-                <li>
-                  <a
-				  		  	  href={link.href}
-				  		  	  tabindex="0"
-				  		  	  role="menuitem">{link.a}</a
-				  		    >
-                </li>
-				  	  {/each}
-				      </ul>      
+      </div>
+      <div class="e">
+          <div class="ee">
+            <div class="other">
+              <input id="input" type="checkbox" checked>
+              <span>Other
+                <svg class="w-5 h-5 -mr-1" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <div class="list">
+                <ul>
+				    	  {#each otherLinks as link}
+                  <li>
+                    <a
+				    		  	  href={link.href}
+				    		  	  tabindex="0"
+				    		  	  role="menuitem">{link.a}</a
+				    		    >
+                  </li>
+				    	  {/each}
+				        </ul>      
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      </div>
-
       <a class="btn" href="/docs/signup">Create an account</a>
     </div>
   </nav>
@@ -198,12 +197,12 @@
 </template>
 
 <style lang="postcss">
-	.a-btn {
+	/* .a-btn {
 		@apply duration-500 block border-b-4 border-blue-300 hover:border-teal-500 mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-teal-800 mr-2;
 	}
 	.dropdown:focus-within .dropdown-menu {
 		opacity: 1;
 		transform: translate(0) scale(1);
 		visibility: visible;
-	}
+	} */
 </style>
